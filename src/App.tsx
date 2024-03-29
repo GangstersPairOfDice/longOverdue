4e6f4c60c8f9057111566e60b27b9c53687f0463
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-03-29 19:37:04 +0100

    cleaned up ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <h1 className="underline">long0verdue</h1>
       <TodoList />
-      <h1>long0verdue</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 5)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> deez nutz!!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
