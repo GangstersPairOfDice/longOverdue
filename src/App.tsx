9f49e3a38cca6025c77fca3eb79f42440dcf3ce2
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-03-30 00:35:23 +0100

    added link to git repo from main web header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <>
-      <h1 className="underline">long0verdue</h1>
+      <h1>
+        <a
+          href="https://github.com/GangstersPairOfDice/longOverdue"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          long0verdue
+        </a>
+      </h1>
       <TodoList />
     </>
   );
